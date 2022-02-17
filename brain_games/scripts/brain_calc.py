61815449f881ae751c3f760e97ea9f261280e0a1
Liliya Samigullina <lil_1992@mail.ru>
2022-02-17 22:30:12 +0300

    update scripts

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
-from brain_games.common import common_game
+from brain_games.common import run_game
 from brain_games.games.calc import description, get_question_and_answer
 
 
 def main():
-    common_game(get_question_and_answer, description)
+    run_game(get_question_and_answer, description)
 
 
 if __name__ == '__main__':
